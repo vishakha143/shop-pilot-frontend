1a1449b9a488de85956159db1b50efaa66b80e02
vishakha <vishakha@gmail.com>
2026-02-02 18:54:20 +0530

    fix: correct firebase config for google auth

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,12 +1,14 @@
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "onecart-5bfab.firebaseapp.com",
+  projectId: "onecart-5bfab",
+  storageBucket: "onecart-5bfab.firebasestorage.app",
+  messagingSenderId: "322505372130",
+  appId: "1:322505372130:web:00cc9b50ece7b957bcc9bb",
+  measurementId: "G-BBJ58W7E99"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
